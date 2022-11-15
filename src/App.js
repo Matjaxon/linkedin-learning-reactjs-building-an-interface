b7185400a6bb98bd63ec1ce384b91cabd6bdbc95
mjackson <mjackson@twilio.com>
2022-11-14 16:15:52 -0800

    Completed 'Sort and Search' module. Added data for appointments from static file. Added onClick handlers to hide/show Add Appointment fields and Sort By dropdown.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { BiArchive } from "react-icons/bi"
-import Search from "./components/Search";
+import { BiCalendar } from "react-icons/bi"
+import AddAppointment from "./components/AddAppointment"
+import AppointmentInfo from "./components/AppointmentInfo";
+import Search from "./components/Search"
+import appointmentList from "./data.json"
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top" />
         Your Appointments
       </h1>
+      <AddAppointment />
       <Search />
+
+      <ul class="divide-y divide-gray-200">
+        {appointmentList.map(appointment => (
+          <AppointmentInfo 
+            key={appointment.id} 
+            appointment={appointment} 
+          />
+        ))}
+      </ul>
     </div>
   );
 }
